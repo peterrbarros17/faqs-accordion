63876ac2fbe3901ee89303fa5896e078149bb8ac
peterrbarros17 <peterrbarros17@gmail.com>
2023-12-04 16:30:12 -0300

    update background

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Header from "./components/Header"
 function App() {
  
   return (
-    <main className="w-full h-screen flex items-center justify-center bg-background-pattern font-work-sans bg-cover bg-no-repeat bg-center">
+    <main className="w-full h-screen flex items-center justify-center bg-purple-900 font-work-sans bg-cover bg-no-repeat bg-center">
       <section className='p-6 bg-slate-200 rounded-xl max-w-2xl z-10 m-5'>
         <Header title="FAQs - Estágio Front End"/>
         <FAQ/>
